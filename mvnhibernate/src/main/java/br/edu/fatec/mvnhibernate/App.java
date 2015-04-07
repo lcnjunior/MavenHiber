7eaae773cd0656f10659a27cf744a5e633b1277d
lcnjunior <lcnjunior@gmail.com>
2015-04-07 19:29:04 -0300

    commit apos a criacao do archtype

---
+++ mvnhibernate/src/main/java/br/edu/fatec/mvnhibernate/App.java
@@ -0,0 +1,13 @@
+package br.edu.fatec.mvnhibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
